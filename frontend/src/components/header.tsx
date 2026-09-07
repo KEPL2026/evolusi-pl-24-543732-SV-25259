45cd6914337df8d32850e709b590afa0f94ca1fb
mat-te123 <denodelviano@gmail.com>
2026-09-08 03:02:29 +0700

    feat : add header components

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,11 @@
+function header() {
+  return (
+    <header className="w-full h-fit flex flex-row items-center justify-end">
+      <a>Home</a>
+      <a>Profile</a>
+      <a>Project</a>
+    </header>
+  );
+}
+
+export default header;
