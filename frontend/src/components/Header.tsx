7bb26934c54f6d3fd48d7461315900d024ec968f
mat-te123 <denodelviano@gmail.com>
2026-09-08 09:08:51 +0700

    fix : fix header style to white

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className="w-full h-fit flex flex-row items-center justify-end">
+    <header className="w-full h-fit flex flex-row items-center justify-end text-white text-lg gap-4">
       <a>Home</a>
       <a>Profile</a>
       <a>Project</a>
