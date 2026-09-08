d9fb306f6722b2953260773c90c9daf999508c87
mat-te123 <denodelviano@gmail.com>
2026-09-08 14:23:48 +0700

    feat: cdn-image API url GET endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,22 @@
 import express, { type Express, type Request, type Response } from "express";
+import pool from "./db";
+import imageRouter from "./image";
 
 const app: Express = express();
+
+app.get("/health", async (req: Request, res: Response) => {
+  try {
+    const client = await pool.connect();
+    await client.query("SELECT 1");
+    client.release();
+    res.status(200).send("Database connection is healthy");
+  } catch (err) {
+    console.error("Database connection error", err);
+    res.status(500).send("Database connection error");
+  }
+});
+
+app.use("/api/images", imageRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Apa Kabar Dunia ♀");
